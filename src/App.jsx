fc2d38fc5efa30a5d4b4ebcd0b3016581beeff78
Utkarsh Ghanwat <utkarshghanwat45@gmail.com>
2026-01-18 12:48:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './pages/login.jsx'
+import Registration from './pages/Registration.jsx'
+
+function App() {
+  const [currentPage, setcurrentPage] = useState("register")
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isRegistered, setIsRegistered] = useState(false)
+
+  return (
+    <div>
+
+      {currentPage === "register" && <Registration />}
+      {currentPage === "login" && isLoggedIn ? (
+        <h1>Welcome, Admin!</h1>
+      ) : (
+        <Login onLogin={() => setIsLoggedIn(true)} />
+      )}
+    </div>
+  )
+}
+export default App
