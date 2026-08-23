54071c02be4fb898e99554dd6413324a3b5f4528
Utkarsh Ghanwat <utkarshghanwat45@gmail.com>
2026-08-24 01:31:47 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ function App() {
   return (
     <div>
 
-
+      <button>Submit</button>
+      
       {currentPage === "register" && <Registration />}
       {currentPage === "login" && isLoggedIn ? (
         <h1>Welcome, Admin!</h1>
